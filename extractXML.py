350bbc960697ef21346d513b3568c87175dbba7b
Iulia Mustea <iuliaotilia26@yahoo.com>
2017-12-09 18:13:18 +0200

    extractXML added

---
+++ extractXML.py
@@ -0,0 +1,42 @@
+import xml.etree.ElementTree as ET
+
+def extractXML(path)
+
+    tree = ET.parse('eu-001-reg.xml')
+    root = tree.getroot()
+     
+    PDF = [[]]
+    nr_tabel = 0
+    nr_pagina = 0
+    x1 = 0
+    x2 = 0
+    y1 = 0
+    y2 = 0
+    for child in root.iter():
+        #if (child.tag == "table"):
+            #nr_tabel = child.attrib['id']
+        if ( child.tag == "region" ):
+            nr_pagina = child.attrib['page']
+        if ( child.tag == "bounding-box"):
+            x1 = child.attrib['x1']
+            y1 = child.attrib['y1']
+            x2 = child.attrib['x2']
+            y2 = child.attrib['y2']
+            boundaries = [x1, y1, x2, y2]
+            
+            #print nr_pagina, nr_tabel, x1, y1, x2, y2
+            if(len(PDF) == int(nr_pagina)):
+                PDF[int(nr_pagina) - 1].append(boundaries);
+            else:
+                PDF.append([])
+                PDF[int(nr_pagina) - 1].append(boundaries);
+            
+return PDF
+
+       
+    
+
+    
+
+    
+    
